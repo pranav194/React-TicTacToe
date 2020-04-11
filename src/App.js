b6a21d2e2dbe83a7395cb7019b005fdda03206ac
Pranav Gupta <pranavgupta194@gmail.com>
2020-04-11 23:53:04 +0530

    react with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,129 @@
-import React , {useState} from 'react';
-import { Button } from '@material-ui/core';
-import Container from '@material-ui/core/Container';
-import AppBar from '@material-ui/core/AppBar';
-
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+import React from 'react'
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table'
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
+
+const style =(win)=>{
+  const styls = {border : win?"4px solid #ca1414": "2px solid #000"};
+  return styls;
+}
+function Square({value, handleClick, win})
+{
+  return (
+    <Button className ="custombtn"  style = {style(win)} onClick = {handleClick}>{value}</Button>
+  )
+}
+function Board({current,handleClick,win})
+{
+  
+  const renderSquare = (i)=>
+  {
+     const w = win&&win.includes(i)?true:false
+      return (<td><Square  win = {w} value = {current[i]} handleClick ={()=>handleClick(i)}/></td>)
+  };
+  
+  
+  return (
+    <Container style ={{width : "300px"}}>
+    <Table size="sm">
+      <tbody>
+        <tr xs={3} md={3} lg={3}>
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </tr>
+        <tr xs={3} md={3} lg={3}>
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </tr>
+        <tr xs={3} md={3} lg={3}>
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </tr>
+      </tbody>
+    </Table>
+    </Container>
+  )
+}
+function Game(){
+  const [history, setHistory] = useState([{squares : Array(9).fill(null)}]);
+  const [stepnumber, setStepNumber] = useState(0);
+  const current  = history[stepnumber];
+  const [isXTurn, setTurn] = useState(true);
+  const move = history.map((step,move)=>(
+      <Button key = {move} style = {{margin : "2px"}}onClick ={()=>jumpTo(move)}>
+        {move===0?'Restart the Game':`Go to Step ${move}`}
+      </Button>
+  ))
+  const jumpTo = (i)=>{
+    setStepNumber(i);
+    setTurn(i%2===0?true:false);
+  }
+  let status;
+  const winner = calculateWinner(current.squares);
+  if(winner)
+  {
+    status = "Winner: " +(isXTurn?'O':'X') ;
+  }
+  else 
+  {
+    status = "Next Player: "+(isXTurn?'X':'O');
+  }
+  const handleClick = (i)=>
+  {
+    if(current[i]||winner)
+    {
+      return;
+    }
+    else
+    {
+      const newSquare = [...current.squares];
+      newSquare[i] = isXTurn?'X':'O';
+      const newHistory = history.slice(0,stepnumber+1);
+      newHistory.push({squares:newSquare});
+      setHistory(newHistory)
+      setTurn(!isXTurn);
+      setStepNumber(newHistory.length-1);
+    }
+  }
+  
+  return (
+    <div>
+    <Board win ={winner}current = {current.squares} handleClick={handleClick} isXTurn ={isXTurn}/>
+    <br/>
+    <h3>{status}</h3>
+    <br/>
+      <ButtonGroup vertical>
+        {move}
+      </ButtonGroup>
+    </div>
+  )
+}
+export default function App() {
+  return (
+    <React.Fragment>
+      <div className= "App">
+        <Navbar bg = "dark" variant = "dark" className ="navbar navbar-light bg-light">
+          <Navbar.Brand href="#home">Tic Tac Toe</Navbar.Brand>
+        </Navbar>
+        <Container fluid style ={{textAlign:"center", paddingTop:"15px"}}>
+          
+          <br/>
+          <Game/> 
+         < br/>
+        </Container>
+      </div>
+    </React.Fragment>
+  )
+}
 
 function calculateWinner(squares) {
   const lines = [
@@ -18,128 +134,14 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
+    [2, 4, 6]
   ];
-  // console.log(squares)
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] &&squares[b]&&squares[c] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return lines[i];
     }
   }
   return null;
 }
 
-const btnstyles = { marginLeft: "5px",height: "34px"};
-function Square({value,onClick})
-{
-  return (
-    <Button className = "square" 
-     variant="contained" 
-     color="primary" 
-     style = {btnstyles} 
-     onClick={onClick}>{value}
-     </Button>
-  )
-}
-function Board({squares ,handleClick}){
-  const renderSquare = (i)=> (<Square value = {squares[i]} onClick = {()=>{handleClick(i)}}></Square>);
-  return (
-    <div className = "Board">
-      <Grid justify="center"
-      alignItems="center" container spacing= {3} style ={{padding : "15px"}}>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </Grid>
-      <Grid justify="center"
-      alignItems="center" container spacing= {3} style ={{padding : "15px"}} >
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-     </Grid>
-     <Grid justify="center"
-      alignItems="center" container spacing= {3}  style ={{padding : "15px"}} >
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-    </Grid>
-    </div>
-  )
-}
-
-function Game()
-{
-  const [history, setHistory]  = useState([{squares:Array(9).fill(null)}]);
-  const [XisNext , setX] = useState(false);
-  const [stepnumber, setStepNumber] = useState(0);
-  const current = history[stepnumber];
-  
-
-  const jumpTo = (move)=>{
-    setStepNumber(move);
-    setX(move%2!==0);
-  }
-
-  const moves = history.map((step, move) => {
-    const desc = move ?
-      'Go to move #' + move :
-      'Go to game start';
-    return (
-      <li key = {move}>
-        <button onClick={() => jumpTo(move)}>{desc}</button>
-      </li>
-    );
-  });
-
-
-
-  const winner = calculateWinner(current.squares);
-  let status = 'Next player: ' + (XisNext ? 'X' : 'O');
-  if(winner)
-    {
-        status = (`Player ${XisNext?'O':'X'} Won`);
-    }
-  const handleClick = (index)=>{
-    const newHistory = history.slice(0, stepnumber + 1);
-    const current = newHistory[stepnumber];
-    const squares = current.squares.slice();
-    if(!squares[index])
-    {
-      squares[index] = XisNext?'X':'O';
-      setHistory(newHistory.concat([{squares:squares}]));
-      setX(!XisNext);
-      setStepNumber(newHistory.length);
-    }
-  }
-  return (
-      <Container>
-        <br/>
-        <Board XisNext = {XisNext} setX = {setX} squares ={current.squares} handleClick = {handleClick}/> 
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </Container> 
-  )  
-}
-
-function App() {
- 
-  // const [player,setTurn] = useState("Player 1")
-  
-  return (
-    <div className="App">
-      <AppBar position = "static">
-      <Toolbar>
-          <Typography variant="h6">
-            TicTacToe
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Game/>
-    </div>
-  );
-}
-
-export default App;
